348909516633aadebc6fe6ad62418d5dc7685153
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-01-19 16:06:36 +0900

    end修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,6 +16,7 @@ class MessagesController < ApplicationController
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
+    end
   end
 private
 
