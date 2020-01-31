2c8289185c9549ccdf7c91de1ae6bd6f41323e14
nakamura suiru <muuvo70619@yahoo.co.jpgit config --global user.name suirunakamuragit config --global user.email muuvo70619@yahoo.co.jp>
2020-01-31 14:50:01 +0900

    chat-spaceの非同期通信化

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,67 @@
 $(function(){
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-       data: formData,
-       dataType: 'json',
-       processData: false,
-       contentType: false
+      function buildHTML(message){
+        if ( message.image ) {
+          var html =
+           `<div class="main-chat__messageroom__onebox" data-message-id=${message.id}>
+              <div class="main-chat__messageroom__onebox__userinfo">
+                <div class="main-chat__messageroom__onebox__userinfo--name">
+                  ${message.user_name}
+                </div>
+                <div class="main-chat__messageroom__onebox__userinfo--time">
+                  ${message.created_at}
+                </div>
+              </div>
+              <div class="main-chat__messageroom__onebox__text">
+                <p class="main-chat__messageroom__onebox__text--image">
+                  ${message.content}
+                </p>
+                <img class="lower-message__image" src=${message.image} alt="pictweetnew.html.erb ">
+              </div>
+            </div>`
+          return html;
+        } else {
+          var html =
+           `<div class="main-chat__messageroom__onebox" data-message-id=${message.id}>
+              <div class="main-chat__messageroom__onebox__userinfo">
+                <div class="main-chat__messageroom__onebox__userinfo--name">
+                  ${message.user_name}
+                </div>
+                <div class="main-chat__messageroom__onebox__userinfo--time">
+                  ${message.created_at}
+                </div>
+              </div>
+              <div class="main-chat__messageroom__onebox__text">
+                <p class="main-chat__messageroom__onebox__text--image">
+                  ${message.content}
+                </p>
+              </div>
+            </div>`
+            return html;
+        };
+      }
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action');
+  $.ajax({
+    url: url,
+    type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-chat__messageroom').append(html);
+      $('.main-chat__messageroom').animate({ scrollTop: $('.main-chat__messageroom')[0].scrollHeight});
+      $('.new_message')[0].reset();
+      $('.form__submit').prop('disabled', false);
     })
-  });
 
+    .fail(function(data) {
+      alert('メッセージ送信に失敗しました');
+    })
+
+})
 });
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   # version :thumb do
     process resize_to_fit: [800, 800]
-  end
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
